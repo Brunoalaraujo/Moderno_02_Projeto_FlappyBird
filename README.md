ea56f15cfb7e0cc9863128a6cf9a40ab4e87e495
Brunoalaraujo <54328102+Brunoalaraujo@users.noreply.github.com>
2021-08-03 15:09:27 -0300

    Colisão e finalização do projeto

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Moderno_02_Projeto_FlappyBird
+
+Projeto realizado com tecnologias de desenvolvimento Web como HTML, CSS e JavaScript
+
+Focado nos conceitos de:
+- Manipulação DOM;
+- Funções e Funções construtoras;
+- Instanciando Objetos;
--- js/flappy.js
+++ js/flappy.js
@@ -124,6 +124,30 @@ function Progresso() {
 //     passaro.animar()
 // }, 20)
 
+function estaoSobrepostos(elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left
+        && b.left + b.width >= a.left
+    const vertical = a.top + a.height >= b.top
+        && b.top + b.height >= a.top
+    return horizontal && vertical
+}
+
+function colidiu(passaro, barreiras) {
+    let colidiu = false
+    barreiras.pares.forEach(parDeBarreiras => {
+        if (!colidiu) {
+            const superior = parDeBarreiras.superior.elemento
+            const inferior = parDeBarreiras.inferior.elemento
+            colidiu = estaoSobrepostos(passaro.elemento, superior) ||
+                estaoSobrepostos(passaro.elemento, inferior)
+        }
+    })
+    return colidiu
+}
+
 function FlappyBrid() {
     let pontos = 0
 
@@ -145,9 +169,13 @@ function FlappyBrid() {
         const temporizador = setInterval(() => {
             barreiras.animar()
             passaro.animar()
+
+            if (colidiu(passaro, barreiras)) {
+                clearInterval(temporizador)
+            }
         }, 20)
     }
 
 }
 
-// new FlappyBrid().start()
+new FlappyBrid().start()
